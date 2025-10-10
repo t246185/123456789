7d7b207e5697739226acd93553df1cdedd88e428
t246185 <t246185@st.yamagata-u.ac.jp>
2025-10-10 19:08:56 +0900

    Add files via upload

---
+++ 第一回課題.c
@@ -0,0 +1,13 @@
+/**文字を表記するためのプログラム 荒川信**/
+
+#include <stdio.h>
+
+int main()
+{
+/**プリント　文字を表記する**/
+int gamera = 530000;
+    printf("私の戦闘力は%dです。\n\n\n", gamera);
+gamera = 540000;
+    printf("あ、間違えた！！%dです。", gamera);
+    return 0;
+}
